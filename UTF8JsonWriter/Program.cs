df447b07f5a82027a349e1129fc2fe5bc7f848ae
jerometrancart <jerome.trancart@gmail.com>
2023-04-26 15:25:28 +0200

    basics of UTF8JsonWriter done

---
+++ UTF8JsonWriter/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using System.Text.Json;
+
+///OUVRE UN FLUX MEMOIRE
+using var memStream = new MemoryStream();
+using var writer = new Utf8JsonWriter(memStream, new JsonWriterOptions
+{
+    Indented = true
+});
+
+writer.WriteStartObject();
+
+writer.WritePropertyName("prenom");
+writer.WriteStringValue("Jerome");
+
+writer.WritePropertyName("nom");
+writer.WriteStringValue("TRANCART");
+
+writer.WritePropertyName("age");
+writer.WriteNumberValue(33);
+
+writer.WriteEndObject();
+
+///ECRIT DANS LE FLUX MEMOIRE
+writer.Flush();
+
+///REPLACE LE CURSEUR EN POSITION 0 DANS LE FLUX MEMOIRE
+memStream.Position = 0;
+///CONVERTIT LES BYTES DU FLUX MEMOIRE EN TABLEAU DE BYTES PUIS EN STRING UTF8 DECODé
+System.Console.WriteLine(Encoding.UTF8.GetString(memStream.ToArray()));
+
