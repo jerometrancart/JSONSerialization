12ee9f7dd2b0478902acbf0a3d33da74f2baa0e9
jerometrancart <jerome.trancart@gmail.com>
2023-04-26 18:05:44 +0200

    added attributes exemples for json serializer

--- JsonSerializer/Livre.cs
+++ JsonSerializer/Livre.cs
@@ -4,6 +4,14 @@ namespace ExoJsonSerializer
 {
     public class Livre
     {
+
+        ///ATTRIBUTS POSSIBLES POUR INCLURE UNE PROP OU UN MEMBRE OU L'IGNORER DANS LE JSON. PAR DEFAUT, SEULS LES PROP PUBLIQUES SERONT SERIALISEES
+        // [JsonInclude]
+        // [JsonIgnore]
+        // ///DEFINIT LE NOM DE LA PROP DANS LE FLUX JSON
+        // [JsonPropertyName("Nom")]
+        // ///INFO SUPPLEMENTAIRES
+        // [JsonExtensionData] 
         public string Titre { get; set; }
         public string Auteur { get; set; }
         public int NbPages { get; set; }
